7d979631c993e83075faf8bfd607d3eb93b059dc
LostCore <lostcore@gmail.com>
2015-05-25 19:15:47 +0200

    Inserita una struttura sample per gli script

--- public/class-public.php
+++ public/class-public.php
@@ -31,19 +31,32 @@ class Pub {
 	}
 
 	public function styles(){
-		if($this->plugin->is_debug()){
-			//...
-		}else{
-			//...
-		}
+		/*
+         * UNCOMMENT AND EDIT THESE LINES
+         */
+
+		//wp_enqueue_style('wb-sample-style', $this->plugin->get_uri() . 'public/assets/dist/css/wb-sample.min.css');
 	}
 
 	public function scripts(){
-		if($this->plugin->is_debug()){
-			//...
+		/*
+		 * UNCOMMENT AND EDIT THESE LINES
+		 */
+
+		/*if($this->plugin->is_debug()){
+			wp_register_script('wb-sample', $this->plugin->get_uri() . 'public/assets/src/js/bundle.js', array('jquery','backbone','underscore'), false, true);
 		}else{
-			//...
+			wp_register_script('wb-sample', $this->plugin->get_uri() . 'public/assets/dist/js/wb-sample.min.js', array('jquery','backbone','underscore'), false, true);
 		}
+
+		$localize_args = array(
+			'ajax_url' => admin_url('admin-ajax.php'),
+			'blogurl' => get_bloginfo("wpurl"),
+			'isAdmin' => is_admin()
+		);
+
+		wp_localize_script('wb-sample','wbData',$localize_args);
+		wp_enqueue_script('wb-sample');*/
 	}
 
 	public function register_post_type(){}
